06f74509d092277d07ac0d91ad7fcff93a8cf181
dbal0503 <108775943+dbal0503@users.noreply.github.com>
2024-05-18 17:14:17 -0400

    sphere update

--- AtlasAnalyticLabSite/themes/atlastheme/static/js/atlas.js
+++ AtlasAnalyticLabSite/themes/atlastheme/static/js/atlas.js
@@ -148,7 +148,7 @@ import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass
 
 		
 				
-                const helper = new THREE.Mesh(new THREE.IcosahedronGeometry(450, 50), shadermaterial);
+                const helper = new THREE.Mesh(new THREE.IcosahedronGeometry(450, 10), shadermaterial);
 
 
                 group.add(helper);
