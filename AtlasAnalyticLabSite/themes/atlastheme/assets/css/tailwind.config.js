c5a85ee4627894624797c0ebcc04d675d69a4a07
dbal0503 <108775943+dbal0503@users.noreply.github.com>
2024-05-18 21:25:30 -0400

    team

--- AtlasAnalyticLabSite/themes/atlastheme/assets/css/tailwind.config.js
+++ AtlasAnalyticLabSite/themes/atlastheme/assets/css/tailwind.config.js
@@ -12,10 +12,10 @@ module.exports = {
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1000px',
+      'lg': '770px',
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '3000px',
+      '2xl': '1500px',
     },
     
     extend: {      fontSize: {
